098a8ee4016b6cb829362a075646921dfcc02220
Clementine <wtan387@aucklanduni.ac.nz>
2025-01-23 17:27:08 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,54 @@
-let userInput;
-        let poem = [];
+let userInput; 
+let poem = [];
+let stressesList = [];
 
-        function setup() {
-            createCanvas(400, 400);
-            background(220);
+function setup() {
+    createCanvas(400, 400);
+    background(220);
 
-            userInput = createInput();
-            userInput.position(10, 10);
+    userInput = createInput();
+    userInput.position(10, 10);
 
-            let button = createButton('add your poem');
-            button.position(userInput.x + userInput.width + 10, 10);
-            button.mousePressed(newLine);
+    let button = createButton('add your poem');
+    button.position(userInput.x + userInput.width + 10, 10);
+    button.mousePressed(newLine);
+}
+
+function newLine() {
+    let userLine = userInput.value();
+    userInput.value('');
+
+    let words = RiTa.tokenize(userLine);
+    let nouns = [];
+    let nounIndices = [];
+
+
+    for (let i = 0; i < words.length; i++) {
+        let pos = RiTa.pos(words[i])[0];
+        if (pos === 'nn') { 
+            nouns.push(words[i]);
+            nounIndices.push(i);
         }
+    }
 
-        function newLine() {
-            let userLine = userInput.value();
-            userInput.value('');
+    if (nouns.length === 0) {
+        poem.push(userLine);
+    } else {
+        let randomIndex = floor(random(0, nounIndices.length));
+        let randomNoun = RiTa.randomWord('nn');
+        words[nounIndices[randomIndex]] = randomNoun;
 
-            let words = RiTa.tokenize(userLine);
-            let nouns = [];
-            let nounIndices = [];
+        let modifiedLine = RiTa.untokenize(words);
+        poem.push(modifiedLine);
+    }
 
-            for (let i = 0; i < words.length; i++) {
-                let pos = RiTa.pos(words[i])[0];
-                if (pos === 'nn') {
-                    nouns.push(words[i]);
-                    nounIndices.push(i);
-                }
-            }
+    let stresses = words.map(word => RiTa.stresses(word));
+    stressesList.push(stresses.join(' '));
 
-            if (nouns.length === 0) {
-                poem.push(userLine);
-            } else {
-                let randomIndex = floor(random(0, nounIndices.length));
-                let randomNoun = RiTa.randomWord('nn');
-                words[nounIndices[randomIndex]] = randomNoun;
 
-                let modifiedLine = RiTa.untokenize(words);
-                poem.push(modifiedLine);
-            }
-
-            background(220);
-            for (let i = 0; i < poem.length; i++) {
-                text(poem[i], 10, 50 + i * 20);
-            }
-        }
+    background(220);
+    for (let i = 0; i < poem.length; i++) {
+        text(poem[i], 10, 50 + i * 40); 
+        text(`Stresses: ${stressesList[i]}`, 10, 70 + i * 40);
+    }
+}
